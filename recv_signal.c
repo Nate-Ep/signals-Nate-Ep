38b56489d7fecf78822e89bb53913a6e201f6d71
Nathan Eppler <epplern@msoe.edu>
2025-11-04 11:47:42 -0600

    Implemented recv_signal

--- recv_signal.c
+++ recv_signal.c
@@ -4,9 +4,36 @@
 * @date 3 November 2025
 */
 
-/**
+ /**
  * File: recv_signal.c
  * Modified by: Nathan Eppler <epplern@msoe.edu>
  * 
- * Brief summary of program:
+ * Brief summary of program: Waits in an infinite loop doing nothing until a 
+ * SIGUSR1 is recieved. On this signal, the program prints out the sender's PID
+ * and then continues in the loop. The program can be killed via kill -SIGKILL <pid>.
+ *
+ * To Send SIGUSR1: kill -SIGUSR1 <pid>
  */
+
+ #include <signal.h>
+ #include <stdio.h>
+ #include <unistd.h>
+
+ void usr1_handler(int signum, siginfo_t *info, void *context) {
+    printf("Sender PID: %d\n", info->si_pid);
+    fflush(stdout);
+ }
+
+ int main(void) {
+    struct sigaction sa = {0}; //set all struct fields to zero
+    sa.sa_sigaction = usr1_handler;
+    sa.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &sa, NULL);
+
+
+    while (1) {
+        sleep(1);
+    }
+
+    return 0;
+ }
--- send_signal.c
+++ send_signal.c
@@ -10,4 +10,26 @@
  * Modified by: Nathan Eppler <epplern@msoe.edu>
  * 
  * Brief summary of program:
+ *
+ * To Send SIGUSR1: -kill SIGUSR1 <pid>
  */
+
+ #include <signal.h>
+ #include <stdio.h>
+
+ void usr1_handler(int pid) {
+    printf("Sender PID: %d", pid);
+ }
+
+ int main(void) {
+    struct sigaction sa = {0}; //set all struct fields to zero
+    sa.sa_handler = &usr1_handler;
+    sigaction(SIGUSR1, &sa, NULL);
+
+
+    while (true) {
+        sleep(1);
+    }
+
+    return 0;
+ }
