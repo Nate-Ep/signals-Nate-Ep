8ae21f03bb7dccf79bc8cc7578ba91db4c8fc547
Nathan Eppler <epplern@msoe.edu>
2025-11-04 11:13:37 -0600

    Implemented signal_segfault

--- signal_segfault.c
+++ signal_segfault.c
@@ -4,15 +4,30 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Nathan Eppler
  * 
- * Brief summary of modifications:
+ * Brief summary of modifications: Modified the program to print out
+ * "Segmentation violation handled" when the program encounters a SIGSEGV signal.
+ *
+ * This causes the program to infinitely print the handling message as it returns
+ * to the same point where the SIGSEGV signal was sent as the program continues
+ * to perform segmentation violations.
  */
 
 
 #include <stdio.h>
+#include <signal.h>
+
+void segv_handler() {
+    printf("Segmentation violation handled\n");
+}
 
 int main (int argc, char* argv[]) {
+    //set up SIGSEGV handler
+    struct sigaction sa = {0}; //set all struct fields to zero
+    sa.sa_handler = &segv_handler;
+    sigaction(SIGSEGV, &sa, NULL); //NULL val says don't store prev handler assignment
+
     // Declare a null pointer
     int* i = NULL;
 
